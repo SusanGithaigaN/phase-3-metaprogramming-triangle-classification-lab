0b2735a957810a4c726386ec6ae740f5a468cf45
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-02-13 15:44:30 +0300

    First commit

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
   # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+  
+  def initialize(equilateral, isosceles, scalene)
+    @equilateral = equilateral
+    @isosceles = isosceles
+    @scalene = scalene
+  end
+  # kind
+  # if else: sideA==sideB && sideB == sideC return :equilateral
+  # elsif: sideA == sideB || sideA == sideC || sideA || sideB == sideC return :isosceles
+  # else return :scalene
+  def kind
+  if (equilateral == isosceles && isosceles == scalene)
+    :equilateral
+  elsif (equilateral == isosceles || equilateral == scalene || isosceles == scalene)
+    :isosceles
+  else
+    :scalene
+  end
+end
+# end
+
+    class TriangleError < StandardError
+    # triangle error code
+    def message
+      "RangeError"
+    end
+  end
 end
